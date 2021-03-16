b7ff74cea6db1331a3c1c6e98e02254f790c4600
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-16 16:15:49 +0000

    comment

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ function textchange () {
   document.getElementById('buttond').style.backgroundColor = 'red'
   document.getElementById('buttond').style.color = 'blue'
 }
+// These changes are to change the formatting of the paragraph of text inside the button once it is pressed.
