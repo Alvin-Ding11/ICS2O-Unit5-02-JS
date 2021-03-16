f292b7bfadf4c31edf4d2bb1c19b08adb39df614
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-16 16:14:13 +0000

    fixes to js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('buttond').addEventListener('click', textchange)
 
-function textchange() {
+function textchange () {
   document.getElementById('buttond').style.fontSize = '120%'
   document.getElementById('buttond').style.fontFamily = 'garamond, serif'
   document.getElementById('buttond').style.backgroundColor = 'red'
